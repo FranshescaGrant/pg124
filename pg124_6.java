03607d9348faf43b065daa93c00618c1ed2f7c39
FranshescaGrant <franshesca.grant@gmail.com>
2014-10-30 09:25:43 -0400

    added problem

---
+++ pg124_6.java
@@ -0,0 +1,10 @@
+class pg124_6{
+	public static void main (String args []) {
+		for (int number = 1; number <= 7; number++) {
+			for (int num = 1; num <= number; num++) {
+			System.out.print(number);
+			} 
+			System.out.println();
+		}
+	}
+}
